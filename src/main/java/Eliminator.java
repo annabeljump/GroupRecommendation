443d88a57f99548317caa291f3af05a7a7a0e78b
annabeljump <annabel.jump@gmail.com>
2016-08-31 23:53:26 +0100

    Added movie list unpacker method

--- src/main/java/Eliminator.java
+++ src/main/java/Eliminator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by annabeljump
@@ -15,6 +16,34 @@ public class Eliminator {
     private UserGroup users;
     private Map<Long, List<ScoredId>> recommendations = new HashMap<>();
     private List<Long> userList = new ArrayList<>();
+    private List<Long> movies = new ArrayList<>();
+
+
+    /**
+     * This method is substantially the same as getMovies() in AgeRestrictor
+     * This is because we need to unpack the Map of recommendations.
+     * Here I have used two transient variables within the method,
+     * which will enable the class field to be updated.
+     */
+    public void getRecommendedMovies() {
+
+        List<List<ScoredId>> interimList = new ArrayList<>();
+        List<ScoredId> interimList2;
+
+        //Get the ScoredId lists out of the Map
+        for(Map.Entry<Long, List<ScoredId>> entry : recommendations.entrySet()) {
+            interimList.add(entry.getValue());
+        }
+
+        //FLATTEN the List<List<ScoredId>>
+        interimList2 = interimList.stream().flatMap(l -> l.stream()).collect((Collectors.toList()));
+
+        //Get the IDs
+        for(ScoredId item : interimList2) {
+            movies.add(item.getId());
+        }
+
+    }
 
 
     //Constructor
