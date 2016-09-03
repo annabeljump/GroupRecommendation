bb002d5bc4cb5e83427e781986370cd1a0870ada
annabeljump <annabel.jump@gmail.com>
2016-09-03 18:06:28 +0100

    added two new classes for next stage of algorithm, the weighting

---
+++ src/main/java/CommonDenominator.java
@@ -0,0 +1,7 @@
+/**
+ * Created by annabeljump
+ * Class that checks for common recommendations and averages ratings
+ * Then adds to a movie list.
+ */
+public class CommonDenominator {
+}
---
+++ src/main/java/GroupWeight.java
@@ -0,0 +1,7 @@
+/**
+ * Created by annabeljump
+ * Class to generate and calculate weighted ratings
+ * of movies not recommended to all users in group
+ */
+public class GroupWeight {
+}
