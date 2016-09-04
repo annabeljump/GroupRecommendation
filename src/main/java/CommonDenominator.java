819a9889d77814c65454727c858ca79f335c8bf0
annabeljump <annabel.jump@gmail.com>
2016-09-04 15:25:47 +0100

    added (empty) master method for comparison and average

--- src/main/java/CommonDenominator.java
+++ src/main/java/CommonDenominator.java
@@ -10,11 +10,30 @@ import java.util.Map;
  * Class that checks for common recommendations and averages ratings
  * Then adds to a movie list.
  */
+
 public class CommonDenominator {
 
     private Map<Long, List<ScoredId>> recMap = new HashMap<>();
     private List<ScoredId> originalList = new ArrayList<>();
+    private Map<Long, Long> finalRecs = new HashMap<>();
 
+
+    /**
+     * Method to be the only call needed from this class
+     * Will set up and then run the comparison and average
+     * of the recommended movies
+     * @return Map of movie ID and predicted average rating
+     */
+    public Map allRatedAndAverage() {
+        begin();
+
+        return finalRecs;
+    }
+
+
+    /**
+     * Method to set up the map and list to search for common movies
+     */
     public void begin() {
         //Pull out first list of recommendations
         Map.Entry<Long, List<ScoredId>> entry = recMap.entrySet().iterator().next();
@@ -25,6 +44,7 @@ public class CommonDenominator {
         //Remove that list from the map
         recMap.remove(u);
     }
+
 
     //Constructor
     public CommonDenominator(Map m) {
