331f15fb949eb51042413204ecbfa733e6b2116d
annabeljump <annabel.jump@gmail.com>
2016-09-04 16:03:32 +0100

    finished method checking if all users were recommended movie

--- src/main/java/CommonDenominator.java
+++ src/main/java/CommonDenominator.java
@@ -1,3 +1,4 @@
+import org.grouplens.lenskit.scored.PackedScoredIdList;
 import org.grouplens.lenskit.scored.ScoredId;
 
 import java.util.ArrayList;
@@ -16,6 +17,8 @@ public class CommonDenominator {
     private Map<Long, List<ScoredId>> recMap = new HashMap<>();
     private List<ScoredId> originalList = new ArrayList<>();
     private Map<Long, Long> finalRecs = new HashMap<>();
+    private Boolean isCommon = false;
+    private List<ScoredId> commonRec = new ArrayList<>();
 
 
     /**
@@ -26,7 +29,7 @@ public class CommonDenominator {
      */
     public Map allRatedAndAverage() {
         begin();
-
+        isAllRecommended();
 
         return finalRecs;
     }
@@ -36,7 +39,21 @@ public class CommonDenominator {
      * Method to check if movie contained in all recommendations
      */
     public void isAllRecommended() {
+        //Get ScoredId
+        for(int i = 0; i < originalList.size(); i++) {
+            Boolean isPresent = true;
 
+            //Check if it is present in all other lists
+            //TODO I don't think this will reset the boolean properly
+            for(Map.Entry<Long, List<ScoredId>> entry : recMap.entrySet()) {
+                if(!entry.getValue().contains(originalList.get(i))) {
+                    isPresent = false;
+                }
+            }
+            if(isPresent) {
+                commonRec.add(originalList.get(i));
+            }
+        }
     }
 
     /**
