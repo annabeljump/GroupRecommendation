cf813a06fe0819e0f92551ba58ab5b9f6d09dd24
annabeljump <annabel.jump@gmail.com>
2017-08-12 17:56:48 +0100

    Created user group, made recommendations

--- src/main/java/RecMethod1.java
+++ src/main/java/RecMethod1.java
@@ -1,11 +1,18 @@
+import org.grouplens.lenskit.scored.ScoredId;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by annabeljump
  * Class to create and run the First of
  * Two group recommendation algorithms
  */
 public class RecMethod1 implements GroupRecGenerator {
-    @Override
-    public void recommendMovies() {
+
+    private List<Long> userList = new ArrayList();
 
     //Step 1: Invididual Recommendations
 
@@ -20,8 +27,39 @@ public class RecMethod1 implements GroupRecGenerator {
 
 
     //Step 5: generate ratings for all other movies - add to group profile
-        //This INCLUDES WEIGHTING
+    //This INCLUDES WEIGHTING
 
 
+    @Override
+    public void recommendMovies() {
+
+        //Step 1a: Put all users in the group into a UserGroup
+
+        System.out.println("Please enter the user IDs of the users in your group (press Q when done):");
+        String user = System.console().readLine();
+        if(!user.equals("Q")) {
+            boolean finished = false;
+            do {
+                Long user1 = Long.valueOf(user);
+                userList.add(user1);
+                System.out.println("Next user please (Q to quit):");
+                user = System.console().readLine();
+                if(user.equals("Q")){
+                    finished = true;
+                }
+            } while (!finished);
+        }
+
+        System.out.println("Thank you. Please enter the host's user ID:");
+        String host = System.console().readLine();
+        Long hoster = Long.valueOf(host);
+
+        GroupCreator userGroup = new UserGroup(userList, hoster);
+
+        //Now get their recommendations
+
+        userGroup.getIndividualRecs();
     }
+
+
 }
