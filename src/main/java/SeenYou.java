572d95d3ced495b71dfb7387f9adad19af50757a
annabeljump <annabel.jump@gmail.com>
2017-08-13 17:51:16 +0100

    using a set for rated movies to ensure no duplicates in final "seen" list

--- src/main/java/SeenYou.java
+++ src/main/java/SeenYou.java
@@ -3,10 +3,7 @@ import org.grouplens.lenskit.scored.ScoredId;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -15,14 +12,15 @@ import java.util.stream.Collectors;
  */
 public class SeenYou {
 
-    private Map<Long, List<ScoredId>> userRecs = new HashMap<>();
-    private List<List<ScoredId>> interimList = new ArrayList<>();
-    private List<ScoredId> interimList2 = new ArrayList<ScoredId>();
-    private List<Long> movieRecList = new ArrayList<>();
+    private Map<Long, List<ScoredId>> userRecs = new HashMap();
+    private List<List<ScoredId>> interimList = new ArrayList();
+    private List<ScoredId> interimList2 = new ArrayList();
+    private List<Long> movieRecList = new ArrayList();
     private UserGroup users = new UserGroup();
-    private List<Long> userList = new ArrayList<Long>();
-    private List<Long> unseenMovies = new ArrayList<Long>();
-    private Map<String, String> userRatingMap = new HashMap<>();
+    private List<Long> userList = new ArrayList();
+    private List<Long> unseenMovies = new ArrayList();
+    private Map<String, String> userRatingMap = new HashMap();
+    private HashSet<Long> seenMovies = new HashSet();
 
 
     /**
@@ -83,8 +81,14 @@ public class SeenYou {
             e.printStackTrace();
         }
 
+        //Now make a List of movies per user and add each movie rated to
+        //the movieSeen hashset - set ensures no duplicates!
+        for(int i = 0; i < userList.size(); i++) {
+            Long u = userList.get(i);
 
 
+            //this.userAgeList.add(Long.parseLong(userAgeMap.get(Long.toString(u))));
+        }
     }
 
     public void checkAndRemoveSeenMovies(){
