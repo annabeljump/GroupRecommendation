0c8d0c9dc05cb03a3a65318569c7b68dc29fe45c
annabeljump <annabel.jump@gmail.com>
2017-08-19 16:11:35 +0100

    Made hashset of movies recommended to remove repetition

--- src/main/java/WeightingGenerator.java
+++ src/main/java/WeightingGenerator.java
@@ -1,6 +1,7 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.grouplens.lenskit.scored.ScoredId;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by annabeljump
@@ -9,14 +10,44 @@ import java.util.Map;
  */
 public class WeightingGenerator {
 
-    private Map userRecs;
+    private Map<Long, List<ScoredId>> userRecs;
     private UserGroup users;
     private List appropriateMovies;
     private List unseenMovies;
     private Map averagedCommonRecs;
+    private HashSet movieList;
 
-    //Step 5a: HashSet recommended movies?
+    private List<List<ScoredId>> interimList = new ArrayList();
+    private List<ScoredId> interimList2 = new ArrayList();
+    private List<Long> movieRecList = new ArrayList();
 
+    //Main method to call other methods
+    public void youHaveBeenWeighed() {
+        youHaveBeenMeasured();
+    }
+
+
+    public void youHaveBeenMeasured() {
+        //Make a HashSet of recommended movies to remove any repetition
+
+        //Extract info from map of recommendations using method from other classes
+        //Get the ScoredId lists out of the Map
+        for(Map.Entry<Long, List<ScoredId>> entry : userRecs.entrySet()) {
+            this.interimList.add(entry.getValue());
+        }
+
+        //FLATTEN the List<List<ScoredId>>
+        this.interimList2 = interimList.stream().flatMap(l -> l.stream()).collect((Collectors.toList()));
+
+        //Get the IDs
+        for(ScoredId item : interimList2) {
+            movieRecList.add(item.getId());
+        }
+
+        //Convert list into HashSet to get rid of repeated entries
+        this.movieList = new HashSet(movieRecList);
+
+    }
     //Step 5b: Remove seen movies
     //Step 5c: remove age inappropriate movies
     //Step 5d: generate recommendations for all remaining movies for each user
