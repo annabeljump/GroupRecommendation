65a240ededa10104269d8060e7c013bb1cab827a
annabeljump <annabel.jump@gmail.com>
2017-08-20 14:39:10 +0100

    Written all tests for NewRecommender. Now to run them...

--- src/test/NewRecommenderTest.java
+++ src/test/NewRecommenderTest.java
@@ -1,6 +1,9 @@
+import org.grouplens.lenskit.scored.ScoredId;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class NewRecommenderTest {
 
     NewRecommender rec;
+    Double rate = 0.0;
 
     @BeforeEach
     void setUp() {
@@ -26,8 +30,11 @@ class NewRecommenderTest {
     @Test
     void run() {
         rec.run();
+
         //Make sure recommendations are being generated
         assert rec.getActualRecs() != null;
+        assert !rec.getActualRecs().isEmpty();
+
         //Make sure a score for movie 17 is being generated
         assert rec.getScore() != 0.0;
 
@@ -37,10 +44,27 @@ class NewRecommenderTest {
 
     @Test
     void recommend() {
+        List recs = rec.recommend();
+
+        //Make sure recommendations are being generated
+        assert recs != null;
+        assert !recs.isEmpty();
+
+        //Make sure there are 10 recommendations TODO CHANGE IF CHANGE NUMBER OF RECS
+        assert recs.size() == 10;
+
     }
 
     @Test
     void predict() {
+        rate = rec.predict(2L, 17L);
+
+        //Make sure a rating is being generated
+        assert rate != 0.0;
+
+        //User 2 has already rated movie 17 with 5 stars. Predicted rating should match?
+        assert rate == 5.0;
+
     }
 
 }
