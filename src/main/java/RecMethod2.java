8e3e3e62dfb05ac70df7173d8869ab9a62a8b985
annabeljump <annabel.jump@gmail.com>
2017-08-24 22:41:42 +0100

    Steps for method 2

--- src/main/java/RecMethod2.java
+++ src/main/java/RecMethod2.java
@@ -8,9 +8,13 @@ public class RecMethod2 implements GroupRecGenerator{
     public void recommendMovies() {
 
     //Step 1: Create group profile - INCLUDING WEIGHTINGS
-       //Commonly rated movies
-       //Host/Children
-       //Low/High
+        //Step 1a: Pull rated movies for each user into a map *user, list*
+        //Step 1b: check for common ratings + average scores *commonDenominator*
+        //Step 1c: check for ratings below a certain threshold + exaggerate
+        //Step 1d: count high ratings + if over half, exaggerate
+        //Step 1e: count low ratings, same as 1d
+        //Step 1f: host weighting
+        //Step 1g: child weighting
 
 
     //Step 2: Remove age in appropriate movies
